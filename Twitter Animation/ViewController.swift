46c8a874427292d839a9d3a4834b30d1fa2cc845
Rahul <rk1821389@gmail.com>
2023-02-08 22:36:55 +0530

    started project

---
+++ Twitter Animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter Animation
+//
+//  Created by Manoj kumar on 08/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
